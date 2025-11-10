d8f7ff964244277257af009f0ac4a3152f46376a
Mohammad Ayas <mohammadayas@MacBook-Air-M4.local>
2025-11-10 19:12:10 +0600

    added booking delete api

--- index.js
+++ index.js
@@ -91,6 +91,13 @@ async function run() {
         res.send(result);
     })
 
+    // Booking delete 
+    app.delete('/bookings/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await bookingsCollection.deleteOne(query);
+        res.send(result);
+    })
 
 
     await client.db("admin").command({ ping: 1 });
